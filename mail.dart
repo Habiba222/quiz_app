14da74450f1fd0fae302883a9eb1b41ba3d4278c
Habiba <71927867+Habiba222@users.noreply.github.com>
2021-11-04 06:14:44 +0200

    Create mail.dart

---
+++ mail.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'result.dart';
+import 'quiz.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Map<String, dynamic>> _questions = const [
+    {
+      'questionText': 'what is yout favourite color?',
+      'answer': [
+        {'text': 'Black', 'score': '10'},
+        {'text': 'Red', 'score': '5'},
+        {'text': 'Green', 'score': '3'},
+        {'text': 'white', 'score': '1'}
+      ],
+    },
+    {
+      'questionText': 'what is yout favourite Animal?',
+      'answer': [
+        {'taxt': 'Cats', 'score': '3'},
+        {'text': 'Dogs', 'score': '11'},
+        {'text': 'Snakes', 'score': '5'},
+        {'text': 'Loins', 'score': '9'}
+      ],
+    },
+    {
+      'questionText': 'what is yout favourite person',
+      'answer': [
+        {'text': 'Habiba', 'score': '0'},
+        {'text': 'Habiba', 'score': '0'},
+        {'text': 'Habiba', 'score': '0'},
+        {'text': 'Habiba', 'score': '0'},
+      ],
+    }
+  ];
+  var _questionIndex = 0;
+  var _totalscore = 0;
+
+  void _answerQuestion(int score) {
+    _totalscore = _totalscore + score;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("we have more questions !");
+    } else {
+      print("no more questions");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app"),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalscore),
+      ),
+    );
+  }
+}
